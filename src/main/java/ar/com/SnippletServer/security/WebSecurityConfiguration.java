534b2b6d03e6798536c2283b112166ac16e78be6
lequerica <lequerica@Martins-MacBook-Pro.local>
2017-08-05 22:13:41 -0300

    syncro

--- src/main/java/ar/com/SnippletServer/security/WebSecurityConfiguration.java
+++ src/main/java/ar/com/SnippletServer/security/WebSecurityConfiguration.java
@@ -48,7 +48,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 				.antMatchers("/returnCategory").permitAll()
 				.antMatchers("/deleteCategory").permitAll()
 				.antMatchers("/getHtmlCategoria").permitAll()
-				.antMatchers("/migracion").permitAll()
+				.antMatchers("/iniciarMigracion").permitAll()
 				.antMatchers("/getNumber").permitAll()
 				.antMatchers("/login").permitAll()
 				.antMatchers("/registration").permitAll()
